3f8dab1df3574252eae8fb7a5abcd4b3aa51ffe8
Ayrton E <aryton.94.e@gmail.com>
2023-01-07 14:32:34 -0600

    Add project files.

---
+++ AppRunner/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+
+string[] arguments = Environment.GetCommandLineArgs();
+if(arguments.Length > 1)
+{
+    if(arguments.Length == 2)
+    {
+        Process.Start(arguments[1]);
+    }else if(arguments.Length == 3)
+    {
+        Process.Start(arguments[1], arguments[2]);
+    }
+}
